31c0a1dcaeeefb94b104c348d6928860decb1f61
Eddie Lin <eddielin0926@gmail.com>
2024-03-07 15:13:56 +0800

    refactor: :building_construction: adjust project structure

--- build.ts
+++ build.ts
@@ -1,23 +1,84 @@
-import manifest from "./manifest.json";
+import { Command } from "commander";
+import { watch } from "fs";
 
-const SRCDIR = "src";
-const OUTDIR = "dist";
+import manifest from "./manifest";
 
-// content_scripts
-const scripts = manifest.content_scripts;
-scripts.map(async (script) => {
-  await Bun.build({
-    entrypoints: script.js.map((path) => `${SRCDIR}/${path}`),
-    outdir: `${OUTDIR}/scripts`,
+type BuildConfig = {
+  publicdir: string;
+  sourcedir: string;
+  outdir: string;
+  manifest: chrome.runtime.Manifest;
+};
+
+const build = async (config: BuildConfig) => {
+  // content_scripts
+  const scripts = config.manifest.content_scripts;
+  if (!scripts) {
+    console.error("No content scripts found in manifest");
+    return;
+  }
+  scripts.map(async (script) => {
+    if (!script.js) {
+      console.error("No js files found in content script");
+      return;
+    }
+    await Bun.build({
+      entrypoints: script.js.map((path) => `${config.sourcedir}/${path}`),
+      outdir: `${config.outdir}`,
+    });
   });
-});
 
-// icons
-const icons = manifest.icons;
-Object.entries(icons).map(async ([size, path]) => {
-  const file = Bun.file(path);
-  await Bun.write(`${OUTDIR}/${path}`, file);
-});
+  // icons
+  const icons = config.manifest.icons;
+  if (!icons) {
+    console.error("No icons found in manifest");
+    return;
+  }
+  Object.entries(icons).map(async ([size, path]) => {
+    const file = Bun.file(`${config.publicdir}/${path}`);
+    await Bun.write(`${config.outdir}/${path}`, file);
+  });
 
-// manifest.json
-await Bun.write(`${OUTDIR}/manifest.json`, JSON.stringify(manifest));
+  // manifest.json
+  await Bun.write(
+    `${config.outdir}/manifest.json`,
+    JSON.stringify(config.manifest)
+  );
+};
+
+const program = new Command();
+
+program
+  .option("--watch", "Watch for file changes", false)
+  .option("--source <dir>", "Source directory", "src")
+  .option("--out <dir>", "Output directory", "dist");
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+const buildConfig: BuildConfig = {
+  publicdir: "public",
+  sourcedir: options.source,
+  outdir: options.out,
+  manifest: manifest,
+};
+
+if (options.watch) {
+  console.log("Watching for file changes");
+  const watcher = watch("./src", { recursive: true }, (event, filename) => {
+    console.log(`Detected ${event} in ${filename}`);
+    build(buildConfig);
+    console.log("Build complete");
+  });
+  process.on("SIGINT", () => {
+    // close watcher when Ctrl-C is pressed
+    console.log("Closing watcher...");
+    watcher.close();
+    process.exit(0);
+  });
+} else {
+  console.log("Building...");
+  build(buildConfig);
+  console.log("Build complete");
+}
---
+++ manifest.ts
@@ -0,0 +1,23 @@
+import packageJson from "./package.json";
+
+const manifest: chrome.runtime.Manifest = {
+  manifest_version: 3,
+  name: "Kaggle Plus",
+  version: packageJson.version,
+  description: packageJson.description,
+  icons: {
+    "24": "icons/icon-24.png",
+    "32": "icons/icon-32.png",
+    "48": "icons/icon-48.png",
+    "128": "icons/icon-128.png",
+  },
+  content_scripts: [
+    {
+      run_at: "document_idle",
+      matches: ["https://www.kaggle.com/competitions/*/leaderboard"],
+      js: ["leaderboard.js"],
+    },
+  ],
+};
+
+export default manifest;
