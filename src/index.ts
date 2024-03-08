7a91743bf3e156611f16d3ed67c73fea7006e598
Eddie Lin <eddielin0926@gmail.com>
2024-03-08 14:48:14 +0800

    refactor: :heavy_plus_sign: use url-match-patterns to match

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import match from "@remusao/url-match-patterns";
 import createChart from "./leaderboard";
 
 const BASEURL = "https://www.kaggle.com";
@@ -5,21 +6,13 @@ const BASEURL = "https://www.kaggle.com";
 document.addEventListener("readystatechange", async (event) => {
   if (document.readyState !== "complete") return;
 
-  if (
-    document.URL.match(
-      /^https:\/\/www\.kaggle\.com\/competitions\/[^/]+\/leaderboard$/
-    )
-  ) {
+  if (match(`${BASEURL}/competitions/*/leaderboard`, document.URL)) {
     createChart();
   }
 });
 
 window.navigation.addEventListener("navigatesuccess", (navigateEvent) => {
-  if (
-    document.URL.match(
-      /^https:\/\/www\.kaggle\.com\/competitions\/[^/]+\/leaderboard$/
-    )
-  ) {
+  if (match(`${BASEURL}/competitions/*/leaderboard`, document.URL)) {
     createChart();
   }
 });
